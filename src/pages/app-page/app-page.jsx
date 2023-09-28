568d4c0988a9c1d769af6133f97b4325251d19a0
Артём Корнилов <temka.kornilov@mail.ru>
2023-09-28 12:01:11 +0300

    Добавлены все зависимости в проект

---
+++ src/pages/app-page/app-page.jsx
@@ -0,0 +1,12 @@
+import './app-page.scss';
+
+function AppPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default AppPage;
